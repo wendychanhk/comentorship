9d78d27075d4f72add41a5ba867ddb22ae425647
wendychanhk <wendychanhku@gmail.com>
2015-08-26 20:39:34 +0200

    overlay

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -12,6 +12,17 @@ $(document).ready(function() {
  
 });
 
+$(function() {
+    
+    $("h2")
+        .wrapInner("<span>")
+
+    $("h2 br")
+        .before("<span class='spacer'>")
+        .after("<span class='spacer'>");
+
+});
+
 var ready = function () {
 
     /**
